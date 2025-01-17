2ed93d42a58f14e7ebb8183a5c65ec13234f2758
JOSE RAMON ESCARPA PEREZ <joseramon.escarpa@hotmail.es>
2025-01-17 07:40:36 +0100

    Update TutorialGrid.tsx2

--- src/components/TutorialGrid.tsx
+++ src/components/TutorialGrid.tsx
@@ -79,6 +79,7 @@ export const TutorialGrid = () => {
     <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <h2 className="text-3xl sm:text-4xl font-bold text-center mb-12">
         Recursos que encontrarás en la web
+        </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {tutorials.map((tutorial) => (
           <TutorialCard key={tutorial.title} {...tutorial} />
